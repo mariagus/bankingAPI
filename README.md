9c5692e16ffe067693b8adf2ae246bd4b9966416
Maria Gusova <masha.gu@gmail.com>
2021-06-02 16:18:37 +0100

    add documentation

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# bankingAPI
+# REST API Documentation
+
+| Property | Description                                            |
+| -------- | ------------------------------------------------------ |
+| id       | The unique identifier by which to identify the service |
+| name     | Name of the account holder                             |
+| balance  | Current amount on account                              |
+
+### GET
+
+Returns a list of accounts held by the Bank
+
+| GET /accounts |
+| ------------- |
+
+Returns all accounts held by the Bank
+
+```
+app.get("/accounts", (req, res) => {
+  connectToDb(async (db) => {
+    const collection = db.collection("accounts");
+    const data = await collection.find({}).toArray();
+    res.json(data);
+  });
+});
+```
+
+| GET /accounts/:id |
+| ----------------- |
+
+Returns the specific account associated with a unique id
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+const url = "mongodb://root:password@localhost:27017";
+const dbName = "bank";
+
+const Client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connectToDb = (cb) => {
+  Client.connect((err) => {
+    let db = Client.db(dbName);
+    cb(db);
+  });
+};
+
+app.get("/accounts", (req, res) => {
+  connectToDb(async (db) => {
+    const collection = db.collection("accounts");
+    const data = await collection.find({}).toArray();
+    res.json(data);
+  });
+});
+
+app.get("/accounts/:id", (req, res) => {
+  const idToFind = ObjectId(req.params.id);
+  connectToDb(async (db) => {
+    const collection = db.collection("accounts");
+    const data = await collection.find({ _id: idToFind }).toArray();
+    res.json(data);
+  });
+});
+
+app.post("/accounts", (req, res) => {
+  const dataToSave = {
+    name: req.body.name,
+    balance: req.body.balance,
+  };
+  connectToDb(async (db) => {
+    const collection = db.collection("accounts");
+    const result = await collection.insertOne(dataToSave);
+
+    if (result.insertedCount === 1) {
+      res.send("account added");
+    } else {
+      res.send("fail");
+    }
+  });
+});
+
+app.listen(port);
